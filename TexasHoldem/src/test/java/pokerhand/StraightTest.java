311713cb915407c8b3cffe2847dd18b81fdb9888
ErichLeister <Janek@Janek-Komputer>
2016-01-25 21:15:30 +0100

    Added test for Straight.

---
+++ TexasHoldem/src/test/java/pokerhand/StraightTest.java
@@ -0,0 +1,29 @@
+package pokerhand;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import exceptions.WrongColorException;
+import exceptions.WrongNameException;
+import model.Card;
+import model.Player;
+
+public class StraightTest {
+
+	@Test
+	public void testSubCompare() throws WrongColorException, WrongNameException {
+		Card t1 = new Card("D", "pik");
+		Card t2 = new Card("5", "kier");
+		
+		Player p1 = Mockito.mock(Player.class);
+		Player p2 = Mockito.mock(Player.class);
+
+		ThreeOfAKing op1 = new ThreeOfAKing(p1, t1);
+		ThreeOfAKing op2 = new ThreeOfAKing(p2, t2);
+
+		assertEquals(op1.subCompare(op1, op2), 1);
+	}
+
+}
