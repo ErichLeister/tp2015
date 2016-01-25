fbe573e8cd33d126d6f6223485354322eff85d22
ErichLeister <Janek@Janek-Komputer>
2016-01-25 20:50:49 +0100

    Update for CardComparator. Reimplemented function findNamesIndex.

--- TexasHoldem/src/main/java/model/CardComparator.java
+++ TexasHoldem/src/main/java/model/CardComparator.java
@@ -1,7 +1,10 @@
 package model;
 
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
+import exceptions.NotSameListsLengthException;
 import exceptions.WrongNameException;
 
 public class CardComparator implements Comparator<Card> {
@@ -9,22 +12,27 @@ public class CardComparator implements Comparator<Card> {
 	private int findNamesIndexInArray(String name) throws WrongNameException {
 		int i=0;
 		boolean isFound = false;
-		while(Card.availableNames[i] != name) {
-			i++;
-			isFound = true;
+//		while(Card.availableNames[i] != name) {
+//			i++;
+//			isFound = true;
+//		}
+//		i--;
+		for(i=0 ; i<Card.availableNames.length ; i++) {
+			if(name.equals(Card.availableNames[i])) {
+				isFound = true;
+				break;
+			}
 		}
-		i--;
 		if(isFound)
 			return i;
 		else
 			throw new WrongNameException();
 			
 	}
+	
 	@Override
 	public int compare(Card arg0, Card arg1) {
-		int cardValues[] = {2,3,4,5,6,7,8,9,10,11,12,13,14};
-
-		int index0=-1, index1=-1, val0, val1;
+		int index0=-1, index1=-1;
 		try {
 			index0 = findNamesIndexInArray(arg0.getName());
 			index1 = findNamesIndexInArray(arg1.getName());
@@ -33,19 +41,36 @@ public class CardComparator implements Comparator<Card> {
 			e.getMessage();
 			System.exit(-1);
 		}
-		val0 = cardValues[index0];
-		val1 = cardValues[index1];
-		
-		if(val0 > val1)
+		if(index0 > index1)
 			return 1;
-		else if(val0 < val1)
+		else if(index0 < index1)
 			return -1;
 		else
 			return 0;
 	}
+	
 	//is a successor of b
 	public boolean isSuccessor(Card a, Card b) throws WrongNameException {
 		return ((this.findNamesIndexInArray(a.getName()) - this.findNamesIndexInArray(b.getName())) == 1);
 	}
 	
+	public int compareCardLists(List<Card> l1, List<Card> l2, Comparator<Card> cc) 
+			throws NotSameListsLengthException {
+		
+		if(l1.size() != l2.size())
+			throw new NotSameListsLengthException();
+		
+		Collections.sort(l1, cc);
+		Collections.sort(l2, cc);
+		int outcome = 0;
+		int i=l1.size()-1;
+		
+		do {
+			outcome =cc.compare(l1.get(i), l2.get(i));
+			i--;
+		}
+		while((outcome == 0) && (i > 0));
+		
+		return outcome;
+	}
 }
