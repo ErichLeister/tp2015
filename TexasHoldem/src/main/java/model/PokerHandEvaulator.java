59050f8914a669a439e3c1b08a61f11e29de1f47
ErichLeister <Janek@Janek-Komputer>
2016-01-26 03:16:28 +0100

    Added functions groupCards and findStraightFlush in PokerHandEvaulator with tests.

--- TexasHoldem/src/main/java/model/PokerHandEvaulator.java
+++ TexasHoldem/src/main/java/model/PokerHandEvaulator.java
@@ -1,45 +1,104 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
-import exceptions.LessThanFiveCardsException;
 import pokerhand.PokerHand;
+import pokerhand.StraightFlush;
+
 
 public class PokerHandEvaulator {
-	private List<PokerHand> pokerHans;
+	private List<PokerHand> pokerHand;
 	
 	public PokerHandEvaulator() {
-		this.pokerHans = null;
+		this.pokerHand = null;
 	}
 	
-	private TreeMap<Card, List<Card>> groupCards(List<Card> cards) {
+//	class PackedCards {
+//		Map<String, List<Card>> packedCards;
+//		PackedCards(List<Card> cards) {
+//			this.packedCards = groupCards.
+//		}
+//	}
+	
+	public Map<Card, List<Card>> groupCards(List<Card> cards) {
 		CardComparator cc = new CardComparator();
 		Collections.sort(cards, cc);
-		TreeMap<Card, List<Card>> output = new TreeMap<Card, List<Card>>();
-		String lastCardsName = null;
+		
+		Map<Card, List<Card>> output = new TreeMap<Card, List<Card>>(cc);
 		
 		Iterator<Card> i = cards.iterator();
-		Card key = null;
-		List<Card> values = null;
-		
+		Card c = i.next();
+		Card key = c;
+		String lastCardsName = key.getName();
+
+		output.put(key, new ArrayList<Card>());
+		output.get(key).add(c);
+
 		while(i.hasNext()) {
-			key = i.next();
-			
+			c = i.next();
+			if(lastCardsName.equals(c.getName())) {
+				output.get(key).add(c);
+			}
+			else {
+				key = c;
+				output.put(key, new ArrayList<Card>());
+				output.get(key).add(c);
+				lastCardsName = key.getName();
+			}	
 		}
-		//TODO
-		return null;
+		return output;
 	}
 	
-	PokerHand findStraightflush(List<Card> cards) throws LessThanFiveCardsException {
+	PokerHand findStraightflush(Player p, Map<Card, List<Card>> cards) {
 		CardComparator cc = new CardComparator();
-		Collections.sort(cards, cc);
-		Iterator<Card> i = cards.iterator();
-		//TODO
-		while(true)
-		return null;
+		//Collections.sort((ArrayList)cards, cc);
+		TreeSet<Card> ts = new TreeSet<Card>(cc);
+		ts.addAll(cards.keySet());
+		
+		Iterator<Card> it =ts.descendingIterator(); //((TreeSet<Card>)cards.keySet()).descendingIterator();
+		Iterator<Card> itColor;
+//		List<String> keys = new ArrayList<String>();
+//		keys.addAll(cards.keySet());
+		boolean isFound = false;
+		boolean isFoundColor = false;
+		Card card = null;
+		Card max = it.next();
+		Card last = max;
+		
+		int i = 1;
+		while(it.hasNext() && !isFound) {
+			card = it.next();
+			if(cc.isSuccessor(last, card)) {
+				itColor = cards.get(card).iterator();
+				Card cardInside;
+				
+				while(itColor.hasNext() && !isFoundColor) {
+					cardInside = itColor.next();
+					if(cardInside.getColor().equals(max.getColor())) {
+						i++;
+						if(i==5) isFound = true;
+						isFoundColor = true;
+					}
+				}
+				if(!isFoundColor) {
+					i = 1;
+					max = card;
+				}
+				isFoundColor = false;
+			}
+			else {
+				i = 1;
+				max = card;
+			}
+			last = card;
+		}
+		return (isFound ? new StraightFlush(p, max) : null);
 	}
 	
 }
---
+++ TexasHoldem/src/test/java/model/PokerHandEvaulatorTest.java
@@ -0,0 +1,101 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import exceptions.WrongColorException;
+import exceptions.WrongNameException;
+
+public class PokerHandEvaulatorTest {
+
+	@Test
+	public void testGroupCards() throws NoSuchMethodException, SecurityException,
+	WrongColorException, WrongNameException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		List<Card> l = new ArrayList<Card>();
+
+		PokerHandEvaulator phe = new PokerHandEvaulator();//Mockito.mock(PokerHandEvaulator.class);
+//		
+//		Method m = (PokerHandEvaulator.class.getClass()).getMethod("groupCards", l.getClass());
+//		m.setAccessible(true);
+		
+		l.add(new Card("5", "kier"));
+		l.add(new Card("W", "pik"));
+		l.add(new Card("9", "trefl"));
+		l.add(new Card("5", "karo"));
+		l.add(new Card("2", "kier"));
+		l.add(new Card("5", "pik"));
+		l.add(new Card("W", "kier"));
+
+		Map<Card, List<Card>> output = (TreeMap<Card, List<Card>>) phe.groupCards(l);//(TreeMap<Card, List<Card>>) m.invoke(phe, l);
+
+		List<Card> keys = new ArrayList<Card>();
+
+		keys.addAll(output.keySet());
+		Collections.sort(keys, new CardComparator());
+		
+		List<Card> l0 = output.get(keys.get(0));
+		List<Card> l1 = output.get(keys.get(1));
+		List<Card> l2 = output.get(keys.get(2));
+		List<Card> l3 = output.get(keys.get(3));
+	
+		if(!(l0.size() == 1) && l0.contains(new Card("2", "kier")))
+			fail();
+		if(!(l1.size() == 3) && l1.contains(new Card("5", "kier")) && l1.contains(new Card("5", "pik")) &&
+				l1.contains(new Card("5", "karo")))
+			fail();
+		if(!(l2.size() == 1) && l2.contains(new Card("9", "trefl")))
+			fail();
+		if(!(l3.size() == 2) && l3.contains(new Card("W", "kier")) && l3.contains(new Card("W", "pik")))
+			fail();
+	}
+
+	@Test
+	public void testFindStraightFlush() throws WrongColorException, WrongNameException {
+		List<Card> l1, l2, l3, l4, l5, l6;
+		Card c1, c2, c3, c4, c5, c6, c7;
+		Player p = Mockito.mock(Player.class);
+		PokerHandEvaulator phe = new PokerHandEvaulator();
+		
+		l1 = new ArrayList<Card>();
+		l2 = new ArrayList<Card>();
+		l3 = new ArrayList<Card>();
+		l4 = new ArrayList<Card>();
+		l5 = new ArrayList<Card>();
+		l6 = new ArrayList<Card>();
+		
+		c1 = new Card("5", "pik");
+		c2 = new Card("8", "kier");
+		c3 = new Card("7", "kier");
+		c4 = new Card("9", "kier");
+		c5 = new Card("K", "pik");
+		c6 = new Card("6", "kier");
+		c7 = new Card("5", "kier");
+		
+		l1.add(c1);
+		l1.add(c7);
+		l2.add(c2);
+		l3.add(c3);
+		l4.add(c4);
+		l5.add(c5);
+		l6.add(c6);
+		
+		Map<Card,List<Card>> arg = new TreeMap<Card,List<Card>>(new CardComparator());
+		arg.put(c1, l1);
+		arg.put(c2, l2);
+		arg.put(c3, l3);
+		arg.put(c4, l4);
+		arg.put(c5, l5);
+		arg.put(c6, l6);
+		
+		assertEquals(phe.findStraightflush(p, arg) != null , true);
+	}
+}
