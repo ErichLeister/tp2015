d89548e08f7068a456566698d42a8043e0c774b2
ErichLeister <Janek@Janek-Komputer>
2016-01-28 20:16:12 +0100

    Updated class Player
    Added functions getChips, setChips, setCards, getCards. Added new
    constructor Player(String name), Player(String name, int chips),
    Player(String name, List<Card> cards), Player(String name, List<Card>
    cards, int chips).

--- TexasHoldem/src/main/java/model/Player.java
+++ TexasHoldem/src/main/java/model/Player.java
@@ -1,23 +1,48 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
-	private Deck cardsInHand;
 	private String name;
+	private int chips;
+	private List<Card> cards;
 	
 	public Player(String name) {
-		cardsInHand = null;
+		this.cards = new ArrayList<Card>();
 		this.name = name;
+		this.chips = 0;
+	}
+	public Player(String name, int chips) {
+		this.cards = new ArrayList<Card>();
+		this.name = name;
+		this.chips = chips;
+	}
+	public Player(String name, List<Card> cards) {
+		this.cards = cards;
+		this.name = name;
+		this.chips = 0;
+	}
+	public Player(String name, List<Card> cards, int chips) {
+		this.cards = cards;
+		this.name = name;
+		this.chips = chips;
 	}
 	
-	public Player(Deck cards) {
-		this.cardsInHand = cards;
+	public List<Card> getCards() {
+		return this.cards;
 	}
-	
+	public void setCards(List<Card> cards) {
+		this.cards = cards;
+	}
 	public String getName() {
 		return name;
 	}
 	
-	public Deck getCardsInHand() {
-		return cardsInHand;
+	public int getChips() {
+		return chips;
+	}
+	public void setChips(int chips) {
+		this.chips = chips;
 	}
 }
