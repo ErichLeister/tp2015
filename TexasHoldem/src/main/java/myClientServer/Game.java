812f2c282176d355526a5c80d9cf49aad35b5d90
ErichLeister <Janek@Janek-Komputer>
2016-01-29 13:31:42 +0100

    Quick fix for a Game class.
    Added an argument to Player's constructor.
    Added an iterator for creating a unique player's name in for each loop.
    Added a String argument in sendMessage method.

--- TexasHoldem/src/main/java/myClientServer/Game.java
+++ TexasHoldem/src/main/java/myClientServer/Game.java
@@ -3,22 +3,26 @@ package myClientServer;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.Player;
+
 public class Game {
 	private List<Player> players;
 
 	public Game(List<RealUser> users){
 		players = new ArrayList<Player>();
+		int i = 1;
 		for(RealUser user : users){
-			players.add(new Player(user));
+			players.add(new Player("player " + i, user));
+			i++;
 		}
 	}
 	public void startGame(){
-		players.get(0).a = "player 1";
+		//players.get(0) = "player 1";
 		//players.get(1).a = "player 2";
 		while(1==1)
 		{
 			for(Player player : players){
-				player.sendMessage();
+				player.sendMessage("message");
 			}
 			players.get(0).getAnswer();
 		}
