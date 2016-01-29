ae1b25e1030f5eb5f09e33046c6e67ea93c9e40e
ErichLeister <Janek@Janek-Komputer>
2016-01-29 13:10:00 +0100

    Updated Player in model package.
    Moved Player from package myClientServer to model.
    The old Player from package model was removed.
    Added new fields: chips, playerStateBehavior, cards etc.
    Added set-get methods for above fields.

--- TexasHoldem/src/main/java/model/Player.java
+++ TexasHoldem/src/main/java/model/Player.java
@@ -1,23 +1,54 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import myClientServer.Answer;
+import myClientServer.Message;
+import myClientServer.RealUser;
+import playerState.PlayerState;
+import playerState.PlayerStateBehavior;
+
 public class Player {
-	private Deck cardsInHand;
 	private String name;
+	private int chips;
+	private PlayerStateBehavior playerStateBehavior;
+	private RealUser user;
+	private List<Card> cards;
 	
-	public Player(String name) {
-		cardsInHand = null;
+	public Player(String name, RealUser user){
 		this.name = name;
+		this.user = user;
+		this.chips = 0;
+		this.cards = new ArrayList<Card>();
+		this.playerStateBehavior = PlayerState.INIT.getStateBehavior();
 	}
-	
-	public Player(Deck cards) {
-		this.cardsInHand = cards;
+
+	public void sendMessage(String msg){
+		user.giveMessage(new Message(msg));
 	}
-	
+	public Answer getAnswer(){
+		return user.getAnswer(10);
+	}
+	public List<Card> getCards() {
+		return this.cards;
+	}
+	public void setCards(List<Card> cards) {
+		this.cards = cards;
+	}
 	public String getName() {
-		return name;
+		return this.name;
 	}
-	
-	public Deck getCardsInHand() {
-		return cardsInHand;
+	public int getChips() {
+		return this.chips;
+	}
+	public void setChips(int chips) {
+		this.chips = chips;
+	}
+	public void setPlayerStateBehavior(PlayerStateBehavior playerStateBehavior) {
+		this.playerStateBehavior = playerStateBehavior;
+	}
+	public PlayerStateBehavior getPlayerStateBehavior() {
+		return this.playerStateBehavior;
 	}
 }
