abf5725aa5d092cbd7d00e2cad1198d32a11c14b
ErichLeister <Janek@Janek-Komputer>
2016-02-01 14:09:10 +0100

    Added PokerHand class

--- TexasHoldem/src/main/java/pokerhand/PokerHand.java
+++ TexasHoldem/src/main/java/pokerhand/PokerHand.java
@@ -1,11 +1,5 @@
 package pokerhand;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
-import exceptions.NotSameListsLengthException;
-import model.Card;
 import model.Player;
 
 public abstract class PokerHand {
@@ -19,25 +13,5 @@ public abstract class PokerHand {
 	
 	public Player getPlayer() {
 		return player;
-	}
-	
-	protected int compareCardLists(List<Card> l1, List<Card> l2, Comparator<Card> cc) 
-			throws NotSameListsLengthException {
-		
-		if(l1.size() != l2.size())
-			throw new NotSameListsLengthException();
-		
-		Collections.sort(l1, cc);
-		Collections.sort(l2, cc);
-		int outcome = 0;
-		int i=l1.size()-1;
-		
-		do {
-			outcome =cc.compare(l1.get(i), l2.get(i));
-			i--;
-		}
-		while((outcome == 0) && (i > 0));
-		
-		return outcome;
 	}
 }
